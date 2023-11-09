b82fed20355097dc9d15b6e2e89fa5f51397900e
Rifat Al Mamun <rifat.mamun@bjitgroup.com>
2023-11-09 16:07:40 +0600

    user interface change

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const rootDir = require('./utils/path');
 
 const app = express();
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/admin',adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin',adminData.router);
 app.use(shopRoutes);
 
 // 404 page!

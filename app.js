e6c2c7b40551e84037d34604f3565400f32b50cf
Rifat Al Mamun <rifat.mamun@bjitgroup.com>
2023-11-13 17:30:19 +0600

    wip: connect with sql

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
 const errorController = require('./controllers/error')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const db = require('./utils/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));

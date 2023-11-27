e531f368c3b5dfd5d8f89f2641510ac39595fb71
Rifat Al Mamun <rifat.mamun@bjitgroup.com>
2023-11-27 10:50:08 +0600

    pagination

--- controllers/shop/product.js
+++ controllers/shop/product.js
@@ -1,18 +1,32 @@
 const mongoose = require('mongoose');
 const Product = require('../../models/Product');
+const ITEMS_PER_PAGE = 3;
 
 const index = (req, res, next) => {
-  Product.find()
-  // .select('title price imageUrl -_id')
-  // .populate('userId', 'name email -_id')
-  .then((response)=>{
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments().then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE)
+  }).then((result)=>{
     res.render('shop/product/index', {
-      prods: response?.length > 0 ? response : [],
+      prods: result?.length > 0 ? result : [],
       pageTitle: 'Shop | products',
-      path: '/products'
+      path: '/products',
+      totalProducts: totalItems,
+      currentPage: page,
+      hasNextPage: (ITEMS_PER_PAGE * page) < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page -1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   })
   .catch(err => {
+    console.log(err)
     const error = new Error(err);
     error.httpStatusCode = 500;
     return next(error);
